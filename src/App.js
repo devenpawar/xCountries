f339fd1274f7bae67594c181fce59f088bd0880c
Deven Pawar <devenpawar8@gmail.com>
2024-08-23 19:51:35 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
       setData(response.data);
       setLoading(false);
     } catch (error) {
+      console.log(error);
       setError(error);
       setLoading(false);
     }
@@ -30,22 +31,22 @@ function App() {
 
   return (
     <div className="App">
-      <Grid container style={{ padding: "2rem",  margin:"1rem"}}>
+      <Grid container style={{ padding: "2rem", margin: "1rem" }}>
         {data &&
           data.map((item) => (
-            <Grid 
-              item 
-              xs={10} 
-              sm={6} 
-              md={4} 
-              lg={2} 
-              xl={1.5} 
-              key={item.id} 
-              style={{ 
-                border: "0.5px solid black",  
-                padding: "1rem", 
-                margin: "0.2rem", 
-                textAlign: "center" 
+            <Grid
+              item
+              xs={10}
+              sm={6}
+              md={4}
+              lg={2}
+              xl={1.5}
+              key={item.id}
+              style={{
+                border: "0.5px solid black",
+                padding: "1rem",
+                margin: "0.2rem",
+                textAlign: "center",
               }}
             >
               <img
@@ -53,9 +54,7 @@ function App() {
                 alt={`Flag of ${item.name}`}
                 style={{ height: "100px", width: "100px", objectFit: "cover" }}
               />
-              <p style={{ margin: "8px 0" , fontWeight: "bold"}}>
-                {item.name}
-              </p>
+              <p style={{ margin: "8px 0", fontWeight: "bold" }}>{item.name}</p>
             </Grid>
           ))}
       </Grid>
