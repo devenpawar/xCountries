53f71516f4ea89e31eca66c4385d9f6b0cc7d80f
Deven Pawar <devenpawar8@gmail.com>
2024-08-23 19:43:33 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Grid } from "@mui/material";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchWeather = async () => {
+    try {
+      const response = await axios.get(
+        "https://xcountries-backend.azurewebsites.net/all"
+      );
+      setData(response.data);
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchWeather();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid container style={{ padding: "2rem",  margin:"1rem"}}>
+        {data &&
+          data.map((item) => (
+            <Grid 
+              item 
+              xs={10} 
+              sm={6} 
+              md={4} 
+              lg={2} 
+              xl={1.5} 
+              key={item.id} 
+              style={{ 
+                border: "0.5px solid black",  
+                padding: "1rem", 
+                margin: "0.2rem", 
+                textAlign: "center" 
+              }}
+            >
+              <img
+                src={item.flag}
+                alt={`Flag of ${item.name}`}
+                style={{ height: "100px", width: "100px", objectFit: "cover" }}
+              />
+              <p style={{ margin: "8px 0" , fontWeight: "bold"}}>
+                {item.name}
+              </p>
+            </Grid>
+          ))}
+      </Grid>
     </div>
   );
 }
